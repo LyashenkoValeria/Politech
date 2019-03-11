cde697c34dd3b43eb19ff02d3f1cd5b4c74200e4
LyashenkoValeria <leovaldez00@mai.ru>
2019-03-11 03:07:07 +0300

    Written test for seach, add, neighbours. Tests for remove aren't completed

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.*;
+
 public class BinarySearchTree {
         public class Node {  //Узел дерева. Хранит информацию о предке, левом и правом потомках
             int key;
@@ -13,7 +16,10 @@ public class BinarySearchTree {
             }
         }
 
+
         Node root = null;  //Корень дерева
+
+       ArrayList<Integer> treeAsList= new ArrayList();
 
         public boolean search( int data ) {  //Поиск в дереве
             Node find = root;
@@ -32,7 +38,11 @@ public class BinarySearchTree {
 
 
         public void add(int insert) {  //Добавление в дерево
+            if (search(insert) == true) throw new IllegalArgumentException(); //Пытаемся добавить в дерево существующий элемент
             Node insertNode = new Node(insert);
+
+            treeAsList.add(insert);
+
             if (root == null) {  //Если дерево пустое
                 root = insertNode;
             }
@@ -58,45 +68,77 @@ public class BinarySearchTree {
         }
 
 
-        public void neighbors (int elementKey) { //Выводит информацию о предке и потомках
-            Node element = new Node(elementKey);
-            if (element.parent != null) System.out.println("Parent = "  + element.parent);
-            if (element.left != null) System.out.println("Left child = " + element.left);
-            if (element.right != null) System.out.println("Right child = " + element.right);
+        public ArrayList<Integer> neighbors (int elementKey) { //Выводит информацию о предке и потомках
+            if (search(elementKey) == false) throw new IllegalArgumentException(); //Пытаемся посмотреть соседей у несуществующего элемента
+
+            Node element = null;
+            Node r = root;
+            while (element == null) {
+                if (elementKey == r.key) element = r;
+                else if (elementKey < r.key) r = r.left;
+                else r = r.right;
+            }
+
+            ArrayList<Integer> parentAndChildren = new ArrayList<Integer>();
+
+            if (element.parent != null) parentAndChildren.add(element.parent.key);
+            if (element.left != null) parentAndChildren.add(element.left.key);
+            if (element.right != null) parentAndChildren.add(element.right.key);
+            return parentAndChildren;
         }
 
 
         public void remove (int deleteKey){ //Удаление элемента
-            Node delete = new Node(deleteKey);
+            if (search(deleteKey) == false) throw new IllegalArgumentException(); //Пытаемся удалить из дерева не существующий элемент
+
+            int number = 0;
+            for (int i = 0; i < treeAsList.size(); i++){
+                if (treeAsList.get(i) == deleteKey) number = i;
+            }
+
+            treeAsList.remove(number);
+
+            Node delete = null;
+            Node r = root;
+            while (delete == null) {
+                if (deleteKey == r.key) delete = r;
+                else if (deleteKey < r.key) r = r.left;
+                     else r = r.right;
+            }
+
             Node parentDelete = delete.parent;           //Случай первый. Удаляемый элемент не имеет потомков
             if (delete.left == null && delete.right == null)
-                if (parentDelete.left == delete) parentDelete.left = null;
+                if (parentDelete.left == delete) {
+                    parentDelete.left = null;
+                }
                 else parentDelete.right = null;
 
 
-            if  (delete.left == null || delete.right == null)  //Случай второй. Удаляемый элемент имеет одного потомка
+            else if  (delete.left == null || delete.right == null)  //Случай второй. Удаляемый элемент имеет одного потомка
                 if (delete.left != null) {              //Если есть левый потомок
                     delete.left.parent = parentDelete;  //Родитель удаляемого элемента становится родителем левлго потомка элемента
                     if (delete == parentDelete.left)  parentDelete.left = delete.left; //Удаляемый элемент был слева
                     else parentDelete.right = delete.left; //Удаляемый элемент был справа
                 }
                 else {                                  //Если есть правый потомок
-                    delete.right.parent = parentDelete;//Родитель удаляемого становится родителем правого потомка
+                    delete.right.parent = parentDelete;  //Родитель удаляемого становится родителем правого потомка
                     if (delete == parentDelete.right)  parentDelete.left = delete.right; //Удаляемый элемент был справа
                     else parentDelete.right = delete.right;           //Удаляемый элемент был слева
                 }
 
 
-            if (delete.left != null && delete.right != null) { //Случай третий. Удаляемый элемент имеет двух потомков
+            else if (delete.left != null && delete.right != null) { //Случай третий. Удаляемый элемент имеет двух потомков
                 Node change = nextElem(delete); //Находим элемент идущий по большенству следом за удаляемым (Следующий)
                 delete.key = change.key;        //Заменяем удаляемый элемент Следующим
                 if (change.parent.left == change) change.parent.left = null; //"Открепляем" Следующего от его старого родителя, чтобы избежать повторения
                 else change.parent.right = null;
             }
+
+
         }
 
 
-        public Node nextElem(Node elem) { //Поиск следеющего эелемента. Нужен для третьего случая удаления
+        public Node nextElem(Node elem) { //Поиск следеющего элемента. Нужен для третьего случая удаления
             Node currentRoot = root;
             Node next = null;
             while (currentRoot != null) {
@@ -106,4 +148,5 @@ public class BinarySearchTree {
             }
             return next;
         }
+
 }
--- src/test/java/BinarySearchTreeTest.java
+++ src/test/java/BinarySearchTreeTest.java
@@ -1,10 +1,17 @@
+import org.junit.Before;
+import org.junit.jupiter.api.function.Executable;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import static org.junit.Assert.*;
 
 public class BinarySearchTreeTest {
-
-    @org.junit.Test
-    public void search() {
-        BinarySearchTree BST = new BinarySearchTree();
+    BinarySearchTree BST = new BinarySearchTree();
+    @Before
+    public void Tree(){
         BST.add(8);
         BST.add(3);
         BST.add(1);
@@ -14,55 +21,96 @@ public class BinarySearchTreeTest {
         BST.add(10);
         BST.add(14);
         BST.add(13);
+    }
+
+    public boolean compare (ArrayList<Integer> tree1, List<Integer> tree2){
+        int count = 0;
+        if (tree1.size() != tree2.size()) return false;
+        else {
+            for (int i = 0; i < tree1.size(); i++) {
+                if (tree1.get(i) == tree2.get(i)) count++;
+            }
+            if (count == tree1.size()) return true;
+            else return false;
+        }
+    }
+
+    @org.junit.Test
+    public void search() {
         assertTrue(BST.search(10));
         assertFalse(BST.search(12));
     }
 
     @org.junit.Test
     public void add() {
-        BinarySearchTree BST = new BinarySearchTree();
-        BST.add(8);
-        BST.add(3);
-        BST.add(1);
-        BST.add(6);
-        BST.add(4);
-        BST.add(7);
-        BST.add(10);
-        BST.add(14);
-        BST.add(13);
+        BinarySearchTree BST1 = new BinarySearchTree();
+        BST1.add(8);
+        BST1.add(3);
+        BST1.add(1);
+        BST1.add(6);
+        BST1.add(4);
+        BST1.add(7);
+        BST1.add(10);
+        BST1.add(14);
+        BST1.add(13);
+        BST1.add(16);
+
+        BinarySearchTree BST2 = new BinarySearchTree();
+        BST2.add(3);
+        BST2.add(8);
+        BST2.add(1);
+        BST2.add(6);
+        BST2.add(4);
+        BST2.add(7);
+        BST2.add(10);
+        BST2.add(14);
+        BST2.add(13);
+        BST2.add(16);
+
+        BST.add(16);
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            public void execute() throws Throwable {
+                BST.add(1);
+            }
+        });
+        assertTrue(compare(BST1.treeAsList, BST.treeAsList));
+        assertFalse(compare(BST2.treeAsList, BST.treeAsList));
     }
 
     @org.junit.Test
     public void neighbors() {
-        BinarySearchTree BST = new BinarySearchTree();
-        BST.add(8);
-        BST.add(3);
-        BST.add(1);
-        BST.add(6);
-        BST.add(4);
-        BST.add(7);
-        BST.add(10);
-        BST.add(14);
-        BST.add(13);
-        BST.neighbors(7);
-        BST.neighbors(1);
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            public void execute() throws Throwable {
+                BST.neighbors(20);
+            }
+        });
+        ArrayList<Integer> check = new ArrayList<Integer>();
+        check.add(3);
+        assertTrue(compare(BST.neighbors(1), check));
+        assertFalse(compare(BST.neighbors(8), check));
     }
 
     @org.junit.Test
     public void remove() {
-        BinarySearchTree BST = new BinarySearchTree();
-        BST.add(8);
-        BST.add(3);
-        BST.add(1);
-        BST.add(6);
-        BST.add(4);
-        BST.add(7);
-        BST.add(10);
-        BST.add(14);
-        BST.add(13);
+        BinarySearchTree BSTWithRemove = new BinarySearchTree();
+        BSTWithRemove.add(8);
+        BSTWithRemove.add(3);
+        BSTWithRemove.add(10);
+        BSTWithRemove.add(14);
+        BSTWithRemove.add(1);
+        BSTWithRemove.add(6);
+        BSTWithRemove.add(4);
+        BSTWithRemove.add(7);
         BST.remove(13);
         BST.add(12);
         BST.remove(14);
         BST.remove(6);
+        BST.add(15);
+
+        assertTrue(compare(BSTWithRemove.treeAsList, BST.treeAsList));
+
+        BinarySearchTree otherBST = new BinarySearchTree();
+        otherBST.add(8);
+        assertFalse(compare(otherBST.treeAsList, BST.treeAsList));
     }
 }
